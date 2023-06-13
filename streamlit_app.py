4878c441cb92188b416846c638e7a4cff1bbae0c
ramya9995 <136402256+ramya9995@users.noreply.github.com>
2023-06-13 09:04:40 +0530

    Update streamlit_app.py
    Added symbols and updated menu with new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents new health diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 and blueberry oatmeal')
-streamlit.text('kale, spinach and oatmeal')
-streamlit.text('Hard-boiled free-range Egg')
+streamlit.text('🥣 Omega3 and blueberry oatmeal')
+streamlit.text('🥗kale, spinach and oatmeal')
+streamlit.text('🐔 Hard-boiled free-range Egg')
+streamlit.text('🥑🍞Acocado toast')
